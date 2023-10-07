d867064290c1162413c009926bfca43908fc6b63
RaihanJL <raihanlaban123@gmail.com>
2023-10-07 18:25:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// if dan if else
+let angka = 10;
+
+if (angka < 5){
+  console.log(angka + " lebih kecil dari 5");
+} else {
+  console.log(angka + " lebih besar dari 5");
+}
+
+// nested if
+let umur = 21;
+if(umur >= 21){
+  console.log("Umur kamu sudah diatas 21, silahkan masuk");
+  if(umur >= 25){
+    console.log("Kamu sudah bisa mengakses situs ini dan menggunakan semua fitur-fitunya")
+  }
+}else{
+  console.log("Kamu masih dibawah umur silahkan keluar dari situs ini")
+}
+
+// 
