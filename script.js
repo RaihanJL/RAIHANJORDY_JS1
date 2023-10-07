e7994cae4a846e5440acde69ff2eb86f98e252e8
RaihanJL <raihanlaban123@gmail.com>
2023-10-07 22:32:35 +0700

    last commit

--- script.js
+++ script.js
@@ -18,4 +18,45 @@ if(umur >= 21){
   console.log("Kamu masih dibawah umur silahkan keluar dari situs ini")
 }
 
-// 
+// Switch case
+let y = 0;
+switch (y){
+  case 0:
+    console.log("Lampu Mati");
+    break;
+  case 1:
+    console.log("Lampu Nyala");
+    break;
+  default:
+    console.log("Lampu Rusak");
+  }
+
+// for
+console.log("Menghitung 1 sampai 5");
+for(let i = 1; i < 6; i++){
+  console.log(i);
+}
+
+// while dan do while
+// WHILE
+const namaTeman = ["Joko", "Riswan", "Nadif", "Budi"];
+let i = 0;
+console.log("Nama Teman Teman Saya :")
+while(namaTeman[i]){
+  console.log(namaTeman[i]);
+  i++
+}
+
+// DO WHILE
+let n = 1;
+do{
+  console.log("angka ke = " + n);
+  n++
+}
+while (n < 11);
+
+// FUNCTION
+let m = perkalian(2, 3);
+function perkalian(a, b){
+  console.log (a * b);
+}
